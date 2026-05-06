1299ca240e1338b5040700378e4fde6dc615d772
Oleksandr KUTSENKO <alexander.kutsenko@avanquest.com>
2026-05-06 18:05:39 +0200

    sort logic

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,6 +1,6 @@
 import cls from './Homepage.module.css';
 import { API_URL } from '../../constants';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 
 import { QuestionCardList } from '../../components/QuestionCardList/QuestionCardList';
 import { Loader } from '../../components/Loader';
@@ -12,6 +12,7 @@ export const Homepage = () => {
     const [questions, setQuestions] = useState([]); //делаем состояние 
 
     const [searchValue, setSearchValue] = useState("");
+    const [sortSelectValue, setSortSelectValue] = useState("");
 
     const [getQuestions, isLoading, error] = useFetch(async (url) => {
         const response = await fetch(`${API_URL}/${url}`); //получили урл
@@ -20,6 +21,13 @@ export const Homepage = () => {
         setQuestions(questions);
         return questions;
     });
+
+    const searchCards = useMemo(() =>{
+        console.log("re-render");
+        return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLowerCase()));
+    }, [questions, searchValue])
+    
+    
 
     // const inputRef = useRef();
 
@@ -39,8 +47,8 @@ export const Homepage = () => {
     // }
 
     useEffect(() => {
-        getQuestions("react");
-    }, []) //в useEffect 1 аргумент коллбек функция, а 2й это массив зависимостей
+        getQuestions(`react?${sortSelectValue}`);
+    }, [sortSelectValue]) //в useEffect 1 аргумент коллбек функция, а 2й это массив зависимостей
 
     // const refTestHandler = () => {
     //     console.dir(inputRef.current.value);
@@ -51,17 +59,30 @@ export const Homepage = () => {
         setSearchValue(evt.target.value);
     }
 
+    const onSortSelectChangeHandler = (evt) => {
+        setSortSelectValue(evt.target.value);
+    }
+
     return (
         <>
             {/* <input type="text" ref={inputRef} />
             <button onClick={refTestHandler}>get ref</button> */}
             <div className={cls.controlsContainer}>
                 <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+                <select name="" id="" value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
+                    <option value="">sort by</option>
+                    <hr />
+                    <option value="_sort=level">level ASC</option>
+                    <option value="_sort=-level">level DESC</option>
+                    <option value="_sort=completed">completed ASC</option>
+                    <option value="_sort=-completed">completed DESC</option>
+                </select>
             </div>
             
             {isLoading && <Loader />} 
             {error && <p>{error}</p>}
-            <QuestionCardList cards={questions} />
+            <QuestionCardList cards={searchCards} />
+            {searchCards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
         </>
     );
 };
