f6e219198cef6c1155449a5cfbd81644fde67569
Oleksandr KUTSENKO <alexander.kutsenko@avanquest.com>
2026-05-08 21:24:05 +0200

    question page, small loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { MainLayout } from './components/MainLayout';
 import { Homepage } from './pages/HomePage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { QuestionPage } from './pages/QuestionPage';
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
           <Route path='/' element={<Homepage />}></Route>
           <Route path='/forbidden' element={<div>Page Not Found 404</div>}></Route>
           <Route path='/addquestion' element={<div>Add Question</div>}></Route>
-          <Route path='/question:id' element={<div>Question page</div>}></Route>
+          <Route path='/question/:id' element={<QuestionPage/>}></Route>
           <Route path='*' element={<NotFoundPage />}></Route>
         </Route>
       </Routes>
