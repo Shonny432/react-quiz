004bb44e9154c9b33684190f1eeeafa76f57d670
Oleksandr KUTSENKO <alexander.kutsenko@avanquest.com>
2026-05-20 15:09:17 +0200

    ts setup

--- __eslint.config.js
+++ __eslint.config.js
@@ -2,25 +2,34 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import prettierPlugin from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js, jsx, ts, tsx}'],
+    files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
-    plugins: {
-      prettier: prettierPlugin
-    },
     languageOptions: {
       globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          printWidth: 130,
+          tabWidth: 2,
+        },
+      ]
+    }
   },
 ])
